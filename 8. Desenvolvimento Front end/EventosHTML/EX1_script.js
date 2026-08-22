4b4b40703f275076907599472697441dcf46b249
Ashfarell <ashfarell@gmail.com>
2026-08-21 23:23:44 -0300

    novo html

---
+++ 8. Desenvolvimento Front end/EventosHTML/EX1_script.js
@@ -0,0 +1,19 @@
+function mudarCor() {
+  document.getElementById('testeScript').style.color = 'grey';
+}
+
+function voltarCor() {
+  document.getElementById('testeScript').style.color = 'black';
+}
+
+function mudarEVoltarCor(cor) {
+  document.getElementById('testeScript1').style.color = cor;
+}
+
+function msgNoClick() {
+  console.log('Botão clicado');
+}
+
+function msgAlerta() {
+  alert('Botão clicado!');
+}
